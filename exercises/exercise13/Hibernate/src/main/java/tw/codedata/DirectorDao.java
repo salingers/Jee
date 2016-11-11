0560eaaec6e5e7123857060aba72c205f93cb6ff
salingers <salingersms@msn.com>
2016-11-11 16:43:52 +0800

    1111

---
+++ exercises/exercise13/Hibernate/src/main/java/tw/codedata/DirectorDao.java
@@ -0,0 +1,10 @@
+package tw.codedata;
+
+import com.google.common.base.Optional;
+
+public interface DirectorDao
+{
+	void saveDirector(Director director);
+
+	Optional<Director> maybeFromName(String name);
+}
---
+++ exercises/exercise13/Hibernate/src/main/java/tw/codedata/DvdDao.java
@@ -0,0 +1,10 @@
+package tw.codedata;
+
+import java.util.List;
+
+public interface DvdDao
+{
+	void saveDvd(Dvd dvd);
+
+	List<Dvd> allDvds();
+}
